00c0c54160b924b9df67d11803dd893308cd7dba
Hyeon-Wu An <zerox2519@gmail.com>
2023-07-06 10:02:46 +0900

    join 관련 매핑 추가

--- src/main/java/zero/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/zero/hello/boot/spring5boot/controller/JoinController.java
@@ -13,10 +13,31 @@ public class JoinController {
     Logger logger = LogManager.getLogger(JoinController.class);
 
     @GetMapping("/agree")
-    public String list() {
+    public String agree() {
         logger.info("join/agree 호출!!");
 
         return "join/agree";
+    }
+
+    @GetMapping("/checkme")
+    public String checkme() {
+        logger.info("join/checkme 호출!!");
+
+        return "join/checkme";
+    }
+
+    @GetMapping("/joinme")
+    public String joinme() {
+        logger.info("join/joinme 호출!!");
+
+        return "join/joinme";
+    }
+
+    @GetMapping("/joinok")
+    public String joinok() {
+        logger.info("join/joinok 호출!!");
+
+        return "join/joinok";
     }
 
 }
