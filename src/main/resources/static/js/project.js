94219e3162a461f370b655c95c68a1a6e6402150
Hyeon-Wu An <zerox2519@gmail.com>
2023-07-06 15:34:21 +0900

    회원가입 : checkme 처리 추가

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -14,3 +14,23 @@ noagree?.addEventListener('click', () => {
 });
 
 
+// checkme
+let frm = document.forms.agreefrm2;
+let checkbtn2 = document.querySelector("#checkbtn2");
+let cancelbtn2 = document.querySelector("#cancelbtn2");
+
+checkbtn2?.addEventListener('click', ()=>{
+    if (frm.name.value === '') alert ('이름을 입력하세요!!');
+    else if (frm.jumin1.value === '') alert ('주민번호를 입력하세요!!');
+    else if (frm.jumin2.value === '') alert ('나머지 주민번호를 입력하세요!!');
+    else if (!frm.infoagree.checked) alert ('주민번호 처리에 동의하세요!!');
+    else {
+        let params = '?name=' + frm.name.value;
+        params += '&jumin1=' + frm.name.value;
+        params += '&jumin2=' + frm.name.value;
+        location.href = '/join/joinme' + params;
+    }
+});
+cancelbtn2?.addEventListener('click', ()=>{
+    location.href = '/';
+});
