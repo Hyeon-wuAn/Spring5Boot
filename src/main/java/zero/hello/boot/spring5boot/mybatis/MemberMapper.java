ccb1a19d427af604167d5dd510d450c4d983eb53
Hyeon-Wu An <zerox2519@gmail.com>
2023-07-07 16:23:04 +0900

    우편번호 조회 : mybatis findZipcode 맵핑 추가

--- src/main/java/zero/hello/boot/spring5boot/mybatis/MemberMapper.java
+++ src/main/java/zero/hello/boot/spring5boot/mybatis/MemberMapper.java
@@ -2,11 +2,13 @@ package zero.hello.boot.spring5boot.mybatis;
 
 import org.apache.ibatis.annotations.Mapper;
 import zero.hello.boot.spring5boot.model.Member;
+import zero.hello.boot.spring5boot.model.Zipcode;
 
 import java.util.List;
 @Mapper
 public interface MemberMapper {
     int insertMember(Member m);
     List<Member> selectMember();
+    List<Zipcode> findZipcode(String dong);
 
 }
